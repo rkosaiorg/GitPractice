a3425e1252509586ca4a38e1d805904647a39ce9
Robert Kosai <rkosaidev@gmail.com>
2020-04-29 10:37:15 -0700

    Update Goo status = 1

--- Assets/Scripts/Goo.cs
+++ Assets/Scripts/Goo.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Goo : MonoBehaviour
 {
+    int status;
     // Start is called before the first frame update
     void Start()
     {
-        
+        status = 1;
     }
 
     // Update is called once per frame
